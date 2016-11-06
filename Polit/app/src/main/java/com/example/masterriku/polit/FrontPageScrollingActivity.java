69a46c14055e652438f1645069bd7d3ebd8704bf
Zach-Attach <4589918@gmail.com>
2016-11-06 01:26:33 -0400

    finally

--- Polit/app/src/main/java/com/example/masterriku/polit/FrontPageScrollingActivity.java
+++ Polit/app/src/main/java/com/example/masterriku/polit/FrontPageScrollingActivity.java
@@ -1,5 +1,6 @@
 package com.example.masterriku.polit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -21,6 +24,7 @@ public class FrontPageScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page_scrolling);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -32,6 +36,8 @@ public class FrontPageScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        new MyAsyncTask().execute("https://www.govtrack.us/api/v2/bill?sort=number");
     }
 
     @Override
@@ -109,5 +115,64 @@ public class FrontPageScrollingActivity extends AppCompatActivity {
         votes/{session}/{chamber}{vote-number}/data.json
 
          */
+    }
+
+    class MyAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String result = null;
+
+            try {
+                URL url = new URL(strings[0]);
+
+                InputStream inputStream = url.openConnection().getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                StringBuilder sb = new StringBuilder(inputStream.available());
+
+                String line = null;
+
+                while ((line = reader.readLine()) != null)
+                {
+//                JSONObject jsonObject = new JSONObject(line);
+//                jsonObject
+                    sb.append(line + "\n");
+//                if (line.substring(0, 20) == "\"introduced_date\":"){
+//                    line.substring(22);
+//             }
+                /*
+                introduced_date
+                    "introduced_date": "2011-01-05",
+                current_status_date
+                    "current_status_date": "2011-01-05",
+                title
+                    "lock_title": false,
+                terms
+
+                 */
+                }
+//            if (sb==null){
+//                return "you messed up";
+//            }
+                //new JSONObject(responseStrBuilder.toString());
+                reader.close();
+                result = sb.toString();//.split("\n");
+            } catch (IOException i){
+                result = i.toString();
+            }
+            return result;
+        }
+
+        protected void onPostExecute(String result) {
+
+//            String page = new Communicator().executeHttpGet("Some URL");
+//            JSONObject jsonObject = new JSONObject(page);
+
+            TextView textView = (TextView) findViewById(R.id.billList);
+            textView.setText(result); //"a regular piece of text"
+//            JSONObject obj = new JSONObject("introduced_date");
+        }
     }
 }
