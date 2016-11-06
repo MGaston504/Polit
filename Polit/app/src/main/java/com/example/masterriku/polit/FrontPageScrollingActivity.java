23335507add46494762cbfe7a14d6b86730937d8
Zach-Attach <4589918@gmail.com>
2016-11-06 08:13:11 -0500

    First Working Versiongit add .

--- Polit/app/src/main/java/com/example/masterriku/polit/FrontPageScrollingActivity.java
+++ Polit/app/src/main/java/com/example/masterriku/polit/FrontPageScrollingActivity.java
@@ -1,21 +1,18 @@
 package com.example.masterriku.polit;
 
-import android.app.ListActivity;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Spinner;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,32 +25,40 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class FrontPageScrollingActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page_scrolling);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        String sort = "current_status_date";
+        //"title_without_number" "introduced_date" 
 
-        String category = "bill?";
-        String sort = "sort=number";
-
-        new MyAsyncTask().execute(String.format("https://www.govtrack.us/api/v2/%s%s", category, sort ));
+        new MyAsyncTask().execute(String.format("https://www.govtrack.us/api/v2/bill?sort=%s", sort));
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -120,7 +125,43 @@ public class FrontPageScrollingActivity extends AppCompatActivity {
          */
     }
 
-    class MyAsyncTask extends AsyncTask<String, Void, String>{
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("FrontPageScrolling Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    class MyAsyncTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... strings) {
@@ -137,13 +178,12 @@ public class FrontPageScrollingActivity extends AppCompatActivity {
 
                 String line = null;
 
-                while ((line = reader.readLine()) != null)
-                {
+                while ((line = reader.readLine()) != null) {
                     sb.append(line + "\n");
                 }
                 reader.close();
                 result = sb.toString();//.split("\n");
-            } catch (IOException i){
+            } catch (IOException i) {
                 result = i.toString();
             }
             return result;
@@ -154,8 +194,9 @@ public class FrontPageScrollingActivity extends AppCompatActivity {
             List<String> titleList = new ArrayList<>();
             List<String> introList = new ArrayList<>();
             List<String> statusList = new ArrayList<>();
+            List<String> webList = new ArrayList<>();
 
-            try{
+            try {
                 JSONObject jsonObject = new JSONObject(result);
                 JSONArray objects = jsonObject.getJSONArray("objects");
 
@@ -164,15 +205,16 @@ public class FrontPageScrollingActivity extends AppCompatActivity {
                     titleList.add(JSONiter.getString("title_without_number"));
                     introList.add(JSONiter.getString("introduced_date"));
                     statusList.add(JSONiter.getString("current_status_date"));
+                    webList.add(JSONiter.getString("link"));
                 }
-            } catch (JSONException j){
+            } catch (JSONException j) {
                 titleList.add(j.toString());
                 introList.add(j.toString());
                 statusList.add(j.toString());
+                webList.add(j.toString());
             }
 
-            MiddleGround(titleList, introList, statusList);
-
+            MiddleGround(titleList, introList, statusList, webList);
 
 
 //            TextView textView = (TextView) findViewById(R.id.billList);
@@ -187,23 +229,23 @@ public class FrontPageScrollingActivity extends AppCompatActivity {
 //            JSONObject obj = new JSONObject("introduced_date");
         }
 
-        public void MiddleGround(List<String> titleList, List<String> introList, List<String> statusList){
-            for(int i = 0; i < (titleList.size() - 1); i++){
+        public void MiddleGround(List<String> titleList, List<String> introList, List<String> statusList, List<String> webList) {
 
+            //View child = getLayoutInflater().inflate(R.layout.content_front_page_scrolling, null);
+            ListView listView = (ListView) findViewById(R.id.BillList);
+            List<String> listItems = new ArrayList<>();
+            //         List<String> listItems = new ArrayList<>();
+
+            for (int i = 0; i < titleList.size(); i++) {
+                listItems.add(String.format("%s\t%s\n\t%s", titleList.get(i), introList.get(i), statusList.get(i)));
             }
 
-            for(int i = 0; i < (introList.size() - 1); i++){
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(FrontPageScrollingActivity.this,
+                    android.R.layout.simple_list_item_1, listItems);
 
-            }
+            listView.setAdapter(adapter);
 
-            for(int i = 0; i < (statusList.size() - 1); i++){
-
-            }
-
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
-            startActivity(browserIntent);
-
-            Spinner s = (Spinner) findViewById(R.id.spinner2);
+            //Spinner s = (Spinner) findViewById(R.id.spinner2);
         }
     }
 }
